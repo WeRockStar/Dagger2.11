57f1aa4994c4608970adefefe75bc1205a6a5cdc
WeRockStar <kotchaphan.m@gmail.com>
2018-05-13 18:35:31 +0700

    Add DI scope

---
+++ app/src/main/java/com/werockstar/dagger212/App.kt
@@ -0,0 +1,11 @@
+package com.werockstar.dagger212
+
+import android.app.Application
+
+class App : Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/werockstar/dagger212/di/scope/PerFragment.kt
@@ -0,0 +1,7 @@
+package com.werockstar.dagger212.di.scope
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class PerFragment
