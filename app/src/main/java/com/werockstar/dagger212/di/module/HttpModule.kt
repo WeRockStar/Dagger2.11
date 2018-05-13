c8ed6087e0f062b34cbbe145b5b791d7b4dc450b
WeRockStar <kotchaphan.m@gmail.com>
2018-05-13 21:37:03 +0700

    Add retrofit config in Dagger2

--- app/src/main/java/com/werockstar/dagger212/di/module/HttpModule.kt
+++ app/src/main/java/com/werockstar/dagger212/di/module/HttpModule.kt
@@ -5,6 +5,8 @@ import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -23,6 +25,8 @@ class HttpModule {
         return Retrofit.Builder()
                 .baseUrl(App.BASE_URL)
                 .client(okHttpClient)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
+                .addConverterFactory(GsonConverterFactory.create())
                 .build()
     }
 }
