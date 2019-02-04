fd83b828e0327a347c50a1ff5336ade75c375fdb
WeRockStar <kotchaphan.m@gmail.com>
2019-02-05 00:24:07 +0700

    Add sample call api

--- app/src/main/java/com/werockstar/dagger211/view/fragment/MainFragment.kt
+++ app/src/main/java/com/werockstar/dagger211/view/fragment/MainFragment.kt
@@ -5,15 +5,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.werockstar.dagger211.api.APIService
 import com.werockstar.dagger212.R
 import com.werockstar.dagger211.view.BaseFragment
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.fragment_main.*
+import javax.inject.Inject
 
 class MainFragment : BaseFragment() {
+
+    @Inject lateinit var api: APIService
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_main, container, false)
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnRequest.setOnClickListener {
+            api.getUser("WeRockStar")
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+
+                    }, {
+
+                    })
+        }
     }
 
 
