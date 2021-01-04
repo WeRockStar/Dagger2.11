47cc71d960baa47000751249eaaa58c9c53c9a8a
WeRockStar <kotchaphan.m@gmail.com>
2021-01-04 09:58:33 +0700

    Update libraries

--- app/src/androidTest/java/com/werockstar/dagger211/view/activity/MainActivityTest.kt
+++ app/src/androidTest/java/com/werockstar/dagger211/view/activity/MainActivityTest.kt
@@ -1,11 +1,11 @@
 package com.werockstar.dagger211.view.activity
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import com.werockstar.dagger211.TestApp
 import com.werockstar.dagger212.R
@@ -25,7 +25,8 @@ class MainActivityTest {
     @Before
     fun setUp() {
         mockWebServer.start()
-        val app = InstrumentationRegistry.getTargetContext().applicationContext as TestApp
+		val context = InstrumentationRegistry.getInstrumentation().targetContext
+		val app = context.applicationContext as TestApp
         app.applyBaseUrl(mockWebServer.url("/").toString())
     }
 
